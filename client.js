5e9bc6dd16c3b4c550c8a159450d9690162bf449
Deric Aaron <aaro0034@umn.edu>
2018-05-15 16:53:18 -0500

    in progress

--- client.js
+++ client.js
@@ -7,14 +7,73 @@ const mayella = { name: 'Mayella', employeeNumber: '89068', annualSalary: '35000
 const employees = [ atticus, jem, scout, robert, mayella ];
 
 class Employee{
-  constructor( name, employeeNumber, annualSalary, reviewRating ){
+  constructor( name, bonusPercentage, totalCompensation, totalBonus ){
     this.name = name;
-    this.employeeNumber = employeeNumber;
-    this.annualSalary = annualSalary;
-    this.reviewRating = reviewRating;
+    this.bonusPercentage = bonusPercentage;
+    this.totalCompensation = totalCompensation;
+    this.totalBonus = totalBonus;
   } // end constructor
 } // end Employee class
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 console.log( employees );
+
+function bonusPerc( rating ){
+  let bonusP = 0;
+
+  if(rating === 3){
+      bonusP = 4;
+  }
+  else if(rating ===4){
+    bonusP = 6;
+  }
+  else if(rating === 5 ){
+    bonusP =  10;
+  }
+}
+
+function bonusVetModifier(totalBonus, number){
+  if(number.length === 4){
+    totalBonus*= 1.05;
+  }
+  return totalBonus;
+}
+
+function bonusSalModifier(totalBonus, salary){
+  if(parseInt(salary) > 65000){
+    totalBonus = totalBonus * (99/100);
+  }
+  return totalBonus;
+}
+
+
+function bonusBounds(totalBonus, salary){
+  let bonusPerc = totalBonus/salary * 100;
+  let boundedBonus = bonusPerc;
+  if(bonusPerc < 0){
+    boundedBonus = 0;
+  }
+  else if(bonusPerc > 13){
+    boundedBonus = 13;
+  }
+  return boundedBonus * salary / 100;
+}
+
+
+let employeesFromClass = [];
+
+function setEmployee( empObject ){
+  let percentInt = bonusPerc(empObject.reviewRating);
+  let salaryInt = parseInt(empObject.annualSalary);
+  let totalBonus = (percentInt/100) * parseInt(empObject.annualSalary);
+  totalBonus = bonusVetModifier(totalBonus, empObject.employeeNumber);
+  totalBonus = bonusSalModifier(totalBonus, salaryInt);
+  totalBonus = bonusBounds(totalBonus, salaryInt);
+  let totalCompensation = totalBonus + salaryInt;
+  employeesFromClass.push(new Employee(empObject.name, percentInt, totalBonus, totalCompensation));
+}
+
+for(employee of employees){
+  setEmployee(employee);
+}
